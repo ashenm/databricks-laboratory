38273f93636e301b45dee30fd10f63ea4dda86cd
Ashen Gunaratne <mail@ashenm.dev>
2026-01-25 16:09:40 +0000

    [environments/aws-private-connectivity] Enable instance profiles

--- environments/aws-private-connectivity/workspaces/main.tf
+++ environments/aws-private-connectivity/workspaces/main.tf
@@ -1,18 +1,19 @@
 locals {
   unity_catalog_bucket_name = lower("${var.name_prefix}-unity-catalog")
   configs                   = merge([for filepath in fileset("${path.module}/config", "*.yaml") : yamldecode(file("${path.module}/config/${filepath}"))]...)
-}
 
-module "clusters" {
-  source      = "../../../modules/clusters"
-  clusters    = local.configs.clusters
-  name_prefix = var.name_prefix
-}
+  clusters = { for key, value in lookup(local.configs, "clusters", {}) : key => merge(value, {
+    instance_profile_arn = lookup(value, "instance_profile", null) != null ? module.instance_profiles.instance_profiles[value.instance_profile].id : null
+  }) }
 
-# module "cluster_policies" {
-#   source   = "../../../modules/cluster-policies"
-#   policies = []
-# }
+  instance_profile_policies = {
+    cloudwatch_agent_serverpolicy = data.aws_iam_policy.cloudwatch_agent_serverpolicy.policy
+  }
+
+  instance_profiles = { for key, value in lookup(local.configs, "instance_profiles", {}) : key => merge(value, {
+    policies = [for idx, policy in value.policies : merge(policy, { policy = local.instance_profile_policies[policy.policy] })]
+  }) }
+}
 
 module "catalogs" {
   source      = "../../../modules/catalogs"
@@ -21,8 +22,21 @@ module "catalogs" {
 }
 
 module "schemas" {
-  source  = "../../../modules/schemas"
-  schemas = lookup(local.configs, "schemas", {})
+  source     = "../../../modules/schemas"
+  schemas    = lookup(local.configs, "schemas", {})
+  depends_on = [module.catalogs]
+}
+
+module "instance_profiles" {
+  source            = "../../../modules/instance-profiles"
+  instance_profiles = local.instance_profiles
+  name_prefix       = var.name_prefix
+}
+
+module "clusters" {
+  source      = "../../../modules/clusters"
+  clusters    = local.configs.clusters
+  name_prefix = var.name_prefix
 }
 
 resource "databricks_default_namespace_setting" "main" {
---
+++ environments/aws-private-connectivity/workspaces/policies.tf
@@ -0,0 +1,3 @@
+data "aws_iam_policy" "cloudwatch_agent_serverpolicy" {
+  name = "CloudWatchAgentServerPolicy"
+}
