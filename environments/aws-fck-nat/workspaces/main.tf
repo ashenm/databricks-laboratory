3b91015ba533a474fdac3fd743367d96b4fcb38c
Ashen Gunaratne <mail@ashenm.dev>
2026-02-03 07:31:18 +0000

    Fix unity catalog securable root storage associations

--- environments/aws-fck-nat/workspaces/main.tf
+++ environments/aws-fck-nat/workspaces/main.tf
@@ -1,12 +1,23 @@
 locals {
   unity_catalog_bucket_name = lower("${var.name_prefix}-unity-catalog")
   configs                   = merge([for filepath in fileset("${path.module}/config", "*.yaml") : yamldecode(file("${path.module}/config/${filepath}"))]...)
+
+  catalogs = { for key, value in lookup(local.configs, "catalogs", {}) : key => merge(value, {
+    storage_root = lookup(value, "storage", null) != null ? "${trimsuffix(module.storages.storage_locations[value.storage].url, "/")}/default" : null
+  }) }
+}
+
+module "storages" {
+  source      = "../../../modules/storages"
+  storages    = lookup(local.configs, "storages", {})
+  name_prefix = var.name_prefix
 }
 
 module "catalogs" {
   source      = "../../../modules/catalogs"
-  catalogs    = lookup(local.configs, "catalogs", {})
+  catalogs    = local.catalogs
   name_prefix = var.name_prefix
+  depends_on  = [module.storages]
 }
 
 resource "databricks_default_namespace_setting" "main" {
